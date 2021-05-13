8415d848f5b93332181474d127c61ecdcb2f345a
MAINAK CHAUDHURI <64016811+MainakRepositor@users.noreply.github.com>
2021-05-13 20:03:51 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -76,7 +76,7 @@ const App = () => {
     <div className="app">
       <div className="app__left">
         <div className="app__header">
-          <h1>COVID-19 TRACKER</h1>
+          <h1>COVID-VIEW</h1>
           <FormControl className="app__dropdown">
             <Select
               variant="outlined"
